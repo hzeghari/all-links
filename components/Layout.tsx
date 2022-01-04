ad8485afe6e0d9bd799eb01e613ac453dd452e3a
Hamza Zeghari <zeghari.hamzaa@gmail.com>
2022-01-05 00:39:49 +0100

    Set main branche

---
+++ components/Layout.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useRef } from 'react'
+import Header from './Header'
+
+
+
+const links = [
+    {
+        label: 'Home',
+    },
+    {
+        label: 'Gallery',
+        isSelected: true,
+    },
+    {
+        label: 'Content',
+    },
+    {
+        label: 'Contact',
+    },
+];
+const ddmItems = [
+    {
+        label: 'Settings',
+    },
+    {
+        label: 'Account',
+    },
+    {
+        label: 'Logout',
+    },
+];
+
+const Layout: React.FC = () => {
+    
+    
+
+    return (
+        <div>
+            <Header 
+            links={links}
+            forceMenuOpenInMobile={true}
+            forceDDMOpenInMobile={false}
+            hideGitHubLink={false}
+            withShadow={false}
+            hideHelp={false}
+            ddmItems={ddmItems}
+            alignRight={false}
+            isFat={false}
+            showSwitchMode={true}/>
+        </div>
+    )
+}
+
+export default Layout
